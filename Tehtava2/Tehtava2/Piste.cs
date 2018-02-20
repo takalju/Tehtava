3cfc21ebd43e9f3dc708704f20a94acbd06c2cd7
k1600741 <k1600741@C-A4404-07>
2018-02-20 12:06:10 +0200

    Eka versio

---
+++ Tehtava2/Tehtava2/Piste.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tehtava2
+{
+    class Piste
+    {
+        public string nimi { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Piste()
+        {
+
+        }
+
+        public Piste(string nimi, double x, double y)
+        {
+            this.nimi = nimi;
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return this.nimi + " " + this.x + " " + this.y;
+        }
+
+    }
+}
---
+++ Tehtava2/Tehtava2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Tehtava2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Piste> pisteet = new List<Piste>();
+
+            Piste eka = new Piste("ekapiste", 1, 2);
+            Piste toka = new Piste("tokapiste", 3, 4);
+
+            pisteet.Add(eka);
+            pisteet.Add(toka);
+
+            string s = JsonConvert.SerializeObject(pisteet);
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            using (FileStream fs = new FileStream(@"C:\Users\k1600741\OneDrive - Epedu O365\Verkko-ohjelmointi\Tentti\Tehtava2\bindataTentti.bin", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    br.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while (br != null && br.BaseStream.Position < br.BaseStream.Length)
+                    {
+                        string p = br.ReadString();
+                        double px = br.ReadDouble();
+                        double py = br.ReadDouble();
+
+                        Piste po = new Piste(p, px, py);
+                        pisteet.Add(po);
+
+
+                    }
+                }
+
+            }
+
+            string s2 = JsonConvert.SerializeObject(pisteet);
+            Console.WriteLine(s2);
+            Console.ReadLine();
+        }
+    }
+}
